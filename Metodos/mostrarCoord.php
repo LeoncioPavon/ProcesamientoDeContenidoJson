982633c351e8b3b5748e693fd0d4d1e7af911101
LeoncioPavon <112792930+LeoncioPavon@users.noreply.github.com>
2023-03-29 13:30:36 -0600

    update MostrarCoord.php

--- Metodos/mostrarCoord.php
+++ Metodos/mostrarCoord.php
@@ -1,19 +1,20 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET"){
-      require_once 'conexion.php' ;
-      $my_query = "select * from coordinador WHERE DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), fechaNac)), '%Y')+0 >= 60";
-      $result = $mysql->query($my_query);
-      if ($mysql->affected_rows > 0) {
-          $json = "{\"data\":[";
-        while ($row = $result->fetch_assoc()){
-            $json = $json.json_encode($row);
-            $json=$json.",";
-        }
-         $json=substr(trim($json),0,-1);
-         $json = $json."]}";
-        }
-echo $json;
-$result->close();
-$mysql->close();
- }
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    require_once 'conexion.php';
+
+    $sql = "SELECT * FROM coordinador WHERE fechaNac<='1963-01-01'";
+    $resultado = $mysql->query($sql);
+
+    $datos = array();
+    while ($row = $resultado->fetch_assoc()) {
+        $datos[] = $row;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($datos);
+
+    $resultado->close();
+}
+
 ?>
